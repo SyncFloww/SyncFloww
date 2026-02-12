614b05d23da0a661e76049e0a80178f4f99b0963
FrankJanePartner <partnermarvel55@gmail.com>
2026-02-12 12:17:01 +0100

    feat: Initialize Django project settings including environment variable loading, security, installed apps, database, REST framework, and JWT configuration.

--- Backend/syncfloww/settings.py
+++ Backend/syncfloww/settings.py
@@ -83,14 +83,21 @@ WSGI_APPLICATION = 'syncflowwAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('POSTGRES_DB', 'postgres'),
+#         'USER': os.getenv('POSTGRES_USERNAME', 'postgres'),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+#         'HOST': os.getenv('POSTGRES_HOST', ''),
+#         'PORT': os.getenv('POSTGRES_PORT', ''),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-        'USER': os.getenv('POSTGRES_USERNAME', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('POSTGRES_HOST', ''),
-        'PORT': os.getenv('POSTGRES_PORT', ''),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
